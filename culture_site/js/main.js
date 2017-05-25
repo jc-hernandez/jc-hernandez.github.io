01c7788f111bfd21962019ab1404905d93347ddf
jc-hernandez <jhernandez@umbel.com>
2017-05-25 18:02:24 -0500

    Final Updates
    Smooth scroll for nav

--- culture_site/js/main.js
+++ culture_site/js/main.js
@@ -20,3 +20,18 @@ $('#previous').on('click', function () {
 	$('.slides').css('transform', 'translateX(' + translateX + '%)');
 });
 // Make it rain!
+
+$('nav a').on('click', function(e) {
+  // prevent the standard link operation on click
+  e.preventDefault();
+  // use the href of the link to identify what
+  // section to scroll to
+  var thisTarget = $(this).attr('href');
+  // get that section's top offset
+  var targetOffset = $(thisTarget).offset().top;
+  // use jQuery.animate() to animate the body's
+  // scrollTop to the targetOffest
+  $('body').animate({
+    scrollTop: targetOffset
+  }, 600);
+});
