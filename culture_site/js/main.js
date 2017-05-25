7e0763d96de5dcb71ee50a2e8c7dc93fc3f00c0a
jc-hernandez <jhernandez@umbel.com>
2017-05-25 16:07:50 -0500

    Updates
    Fixed BG
    Image slider
    new culture

--- culture_site/js/main.js
+++ culture_site/js/main.js
@@ -1,1 +1,22 @@
+var translateX = 0;
+
+$('#next').on('click', function () {
+	if (translateX === -75) {
+		translateX = 0;
+	} else {
+		translateX -= 25;
+	}
+
+	$('.slides').css('transform', 'translateX(' + translateX + '%)');
+});
+
+$('#previous').on('click', function () {
+	if (translateX === 0) {
+		translateX = -75;
+	} else {
+		translateX += 25;
+	}
+
+	$('.slides').css('transform', 'translateX(' + translateX + '%)');
+});
 // Make it rain!
